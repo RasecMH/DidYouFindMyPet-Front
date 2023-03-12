71efd6cd149b0d4cab75b7e61fe30650abe1581e
Cesar Holanda <rasecmh0@gmail.com>
2023-03-12 08:21:11 -0300

    add: About modal content

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -11,34 +11,13 @@ export default function NavMenu() {
       <div className='flex-none'>
         <ul className='menu menu-horizontal px-1'>
           <li>
-            <label htmlFor='help-modal'>Help</label>
+            <label htmlFor='help-modal'>About</label>
           </li>
           <li>
-            <label htmlFor='contact-modal'>Contact Us</label>
-          </li>
-          <li tabIndex={0}>
-            <a>
-              English
-              <svg
-                className='fill-current'
-                xmlns='http://www.w3.org/2000/svg'
-                width='20'
-                height='20'
-                viewBox='0 0 24 24'>
-                <path d='M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z' />
-              </svg>
-            </a>
-            <ul className='p-2 bg-base-100'>
-              <li>
-                <a>Português</a>
-              </li>
-            </ul>
+            <label htmlFor='contact-modal'>Contact</label>
           </li>
           <li>
             <a>Sign Up</a>
-          </li>
-          <li>
-            <img src={houseIcon} />
           </li>
         </ul>
       </div>
@@ -60,10 +39,32 @@ export default function NavMenu() {
         <input type='checkbox' id='help-modal' className='modal-toggle' />
         <label htmlFor='help-modal' className='modal cursor-pointer'>
           <label className='modal-box relative' htmlFor=''>
-            <h3 className='text-lg font-bold'>Help Modal</h3>
+            <h3 className='text-3xl font-bold'>About</h3>
             <p className='py-4'>
-              You've been selected for a chance to get one year of subscription
-              to use Wikipedia for free!
+              The purpose of the project is to allow users to register
+              information about an animal, and generate a QR code that can be
+              scanned to generate a history of the animal's location.
+              <p className='py-4'>
+                The application has a simple and intuitive user interface built
+                using React, which allows users to easily register and manage
+                animal information. The backend of the application is built
+                using Node.js and TypeScript, and stores the animal data in a
+                database.
+              </p>
+              <p className='py-4'>
+                Once an animal has been registered, the application generates a
+                unique QR code for that animal. This QR code can be printed and
+                contains a link to a web page that displays the animal's
+                information and share the location with user, the person who
+                scanned the QR code also can send a message and contact info.
+              </p>
+              <p className='py-4'>
+                Overall, my this project provides a useful tool for animal
+                owners, caretakers, and researchers who need to keep track of an
+                animal's location history. The use of TypeScript, React, and
+                Node.js ensures that the application is reliable, efficient, and
+                easy to maintain.
+              </p>
             </p>
           </label>
         </label>
