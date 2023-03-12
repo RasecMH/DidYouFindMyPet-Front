a1082a949ff975c76a76fa4efbd87cad9bbc86ae
Cesar Holanda <rasecmh0@gmail.com>
2023-03-11 23:45:45 -0300

    style: petCard Modal

--- src/components/PetCard.tsx
+++ src/components/PetCard.tsx
@@ -58,15 +58,19 @@ export default function PetCard({
         <input type='checkbox' id={`${name}-${id}`} className='modal-toggle' />
         <label htmlFor={`${name}-${id}`} className='modal cursor-pointer'>
           <label className='modal-box relative' htmlFor=''>
-            <h3 className='text-lg font-bold'>{name}</h3>
-            <p className='py-4'>{description}</p>
-            <p className='py-4'>{health}</p>
-            <img src={qrCode} />
-            <a href={qrCodeBlob} download={name} target='_blank'>
-              <button type='button' className='btn'>
-                DOWNLOAD
-              </button>
-            </a>
+            <h3 className='text-3xl font-bold mb-5'>{name}</h3>
+            <h1 className='text-lg font-bold'>Description:</h1>
+            <p className='py-2'>{description}</p>
+            <h1 className='text-lg font-bold'>Health:</h1>
+            <p className='py-2'>{health}</p>
+            <div className='flex flex-col gap-6 mt-5 justify-center items-center'>
+              <img src={qrCode} />
+              <a href={qrCodeBlob} download={name} target='_blank'>
+                <button type='button' className='btn'>
+                  DOWNLOAD
+                </button>
+              </a>
+            </div>
           </label>
         </label>
       </div>
